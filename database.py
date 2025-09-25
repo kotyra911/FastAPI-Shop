9e78bb97f7fb3dd59fb4db41fe68960352128789
Климанский Матвей <ak1986tutu@gmail.com>
2025-09-25 15:44:19 +0800

    Исправлены мелкие косяки, добавлен env файл

--- database.py
+++ database.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-# ссылка на бд //логин:пароль@адрес:порт/имя_бд
-# константа, поэтому большими буквами
-SQLALCHEMY_DATABASE_URL: str = 'postgresql+psycopg2://postgres:connectToBase@localhost:5432/Shop'
+load_dotenv()
+
+db_password = os.getenv("DB_PASSWORD")
+db_user = os.getenv("DB_USER")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+db_port = os.getenv("DB_PORT")
+SQLALCHEMY_DATABASE_URL: str = f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 
 # создание движка, содержит соединение с бд
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
